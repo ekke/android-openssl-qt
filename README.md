254c855d136b2b4289315194a542b258311ce4f9
ekkes-corner <ekke@ekkes-corner.org>
2017-03-09 10:50:01 +0100

    works fine with NDK r10e

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # android-openssl-qt
-scripts to generate openssl .so to be used from Qt Android Projects
+scripts to generate openssl .so to be used from Qt Android Projects on MacOS
+
+Building Qt mobile Apps for Android there's a problem if App should run on Android 7+, because Google removed openssl.
+You must build openssl .so libs by yourself
+
+There's a documentation from Qt HowTo add openssl:
+http://doc.qt.io/qt-5/opensslsupport.html 
+
+Unfortunately this fails on MacOS with error 
+unknown argument: '-mandroid'
+
+see also QTBUG-59375  https://bugreports.qt.io/browse/QTBUG-59375 
+
+thx Marco Piccolino and Roman Pasechnik
+in this repo I found the needed scripts to generate the openssl libraries by myself.
+https://github.com/orangefour/android-openssl 
+
+These scripts do all for you from downloading and extracting openssl and generating libs for x86 and armeabi-v7a
+
+I'm not providing any prebuilt .so here as you may found in other repos at github,
+because this would be a security hole to embed .so you don't have built from origin sources.
+
+I did small modifications to the scripts to run them on OSX
+Please check the environment values of setenv-android-mod.sh
+
+## HowTo create libssl.so and libcrypto.so
+Clone this repo to a location you can refer to from your Qt projects.
+
+Open Terminal and do:
+
+cd <path/to/this/repo>
+chmod 755 ./build-all-arch.sh
+chmod 755 ./setenv-android-mod.sh
+export ANDROID_NDK_ROOT=/your/path/to/_android/android-ndk-r10e
+export OPENSSL_VERSION="openssl-1.0.2k"
+./build-all-arch.sh
+
+## Modify .pro
+insert this line into your .pro
+
+include(android-openssl.pri)
+
+## copy android-openssl.pri
+copy this .pri into your projects (don't forget to adjust the path)
+
+android {
+  ANDROID_EXTRA_LIBS += $$PWD/my/path/to/prebuilt/armeabi-v7a/libcrypto.so
+  ANDROID_EXTRA_LIBS += $$PWD/my/path/to/android-openssl/prebuilt/armeabi-v7a/libssl.so
+}
---
+++ build-all-arch.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#
+# http://wiki.openssl.org/index.php/Android
+#
+set -e
+rm -rf prebuilt
+mkdir prebuilt
+
+# OPENSSL_VERSION="openssl-1.0.2j"
+
+curl -O "https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz"
+tar xfz "${OPENSSL_VERSION}.tar.gz"
+
+archs=(armeabi-v7a x86)
+
+for arch in ${archs[@]}; do
+    xLIB="/lib"
+    case ${arch} in
+        "armeabi-v7a")
+            _ANDROID_TARGET_SELECT=arch-arm
+            _ANDROID_ARCH=arch-arm
+            _ANDROID_EABI=arm-linux-androideabi-4.9
+            configure_platform="android-armv7" ;;
+        "arm64-v8a")
+            _ANDROID_TARGET_SELECT=arch-arm64-v8a
+            _ANDROID_ARCH=arch-arm64
+            _ANDROID_EABI=aarch64-linux-android-4.9
+            #no xLIB="/lib64"
+            configure_platform="linux-generic64 -DB_ENDIAN" ;;
+        "mips")
+            _ANDROID_TARGET_SELECT=arch-mips
+            _ANDROID_ARCH=arch-mips
+            _ANDROID_EABI=mipsel-linux-android-4.9
+            configure_platform="android -DB_ENDIAN" ;;
+        "mips64")
+            _ANDROID_TARGET_SELECT=arch-mips64
+            _ANDROID_ARCH=arch-mips64
+            _ANDROID_EABI=mips64el-linux-android-4.9
+            xLIB="/lib64"
+            configure_platform="linux-generic64 -DB_ENDIAN" ;;
+        "x86")
+            _ANDROID_TARGET_SELECT=arch-x86
+            _ANDROID_ARCH=arch-x86
+            _ANDROID_EABI=x86-4.9
+            configure_platform="android-x86" ;;
+        "x86_64")
+            _ANDROID_TARGET_SELECT=arch-x86_64
+            _ANDROID_ARCH=arch-x86_64
+            _ANDROID_EABI=x86_64-4.9
+            xLIB="/lib64"
+            configure_platform="linux-generic64" ;;
+        *)
+            configure_platform="linux-elf" ;;
+    esac
+
+    mkdir prebuilt/${arch}
+
+    . ./setenv-android-mod.sh
+
+    echo "CROSS COMPILE ENV : $CROSS_COMPILE"
+    cd "${OPENSSL_VERSION}"
+
+    xCFLAGS="-DSHARED_EXTENSION=.so -fPIC -DOPENSSL_PIC -DDSO_DLFCN -DHAVE_DLFCN_H -mandroid -I$ANDROID_DEV/include -B$ANDROID_DEV/$xLIB -O3 -fomit-frame-pointer -Wall"
+
+    perl -pi -e 's/install: all install_docs install_sw/install: install_docs install_sw/g' Makefile.org
+    ./Configure shared no-threads no-asm no-zlib no-ssl2 no-ssl3 no-comp no-hw no-engine --openssldir=/usr/local/ssl/android-21/ $configure_platform $xCFLAGS
+
+    # patch SONAME
+
+    perl -pi -e 's/SHLIB_EXT=\.so\.\$\(SHLIB_MAJOR\)\.\$\(SHLIB_MINOR\)/SHLIB_EXT=\.so/g' Makefile
+    perl -pi -e 's/SHARED_LIBS_LINK_EXTS=\.so\.\$\(SHLIB_MAJOR\) \.so//g' Makefile
+    # quote injection for proper SONAME, fuck...
+    perl -pi -e 's/SHLIB_MAJOR=1/SHLIB_MAJOR=`/g' Makefile
+    perl -pi -e 's/SHLIB_MINOR=0.0/SHLIB_MINOR=`/g' Makefile
+    make clean
+    make depend -j2
+    make build_libs -j2
+
+    file libcrypto.so
+    file libssl.so
+    cp libcrypto.so ../prebuilt/${arch}/libcrypto.so
+    cp libssl.so ../prebuilt/${arch}/libssl.so
+    cd ..
+done
+exit 0
+
